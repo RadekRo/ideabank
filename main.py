748342827496186d74c03a5c06c0ed781786d0e5
RadekRo <r.roclawski@ankara.pl>
2023-02-05 11:43:32 +0100

    Added ordered list of user thoughts displayed simultaneously under question

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+ideas = []
 def ideabank():
     while True:
-        input("What is your new idea?: ")
+        user_idea = input("What is your new idea?: ")
+        ideas.append(user_idea)
+        for idea in ideas:
+            number = ideas.index(idea) + 1
+            print(number, ". ", idea, sep = "")
 ideabank()
